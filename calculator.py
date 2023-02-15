7bbaa45d958fbea8222b258475dbcd982df2e2fc
jog <gsakajoshua166@gmail.com>
2023-02-15 11:37:32 +0300

    added error checking

--- calculator.py
+++ calculator.py
@@ -6,13 +6,23 @@ operator=input("enter operatot:")
 
 def add(num1,num2):
     result=num1+num2
-    return result
+    print(result)
 def subtract(num1,num2):
     result=num1-num2
-    return result
+    print(result)
 def divide(num1,num2):
     result=num1/num2
-    return result
+    print(result)
 def multiply(num1,num2):
     result=num1*num2
-    return result
+    print(result)
+if operator=="+":
+    add(number1,number2)
+elif operator=="-":
+    subtract(number1,number2)
+elif operator=="/":
+    divide(number1,number2)
+elif operator=="X" or operator=="x" or operator=="*":
+    multiply(number1,number2)
+else:
+    print("invalid operator")
